2c106b26fa4d017fe429d04be1fefd8c637d0471
ahmetustel <ustelahmett@gmail.com>
2023-01-26 16:19:18 +0300

    Token oluşturma projesi tamamlandı

--- app.js
+++ app.js
@@ -10,11 +10,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-// Token oluştururken kullanılacak global değişken secretKey'i koda dahil ederiz
-app.set("api_secret_key", process.env.api_secret_key);
-
-// app.set("api_secret_key", require("./config").api_secret_key);
-
 /* This is the root route. It is used to check if the server is running. */
 app.get("/", (req, res) => {
     res.status(200).json({ alive: "True" });
--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const PORT = process.env.PORT || 5000;
 
 require('dotenv').config();
 
-const KEY = process.env.api_secret_key;
-app.set("api_secret_key", KEY);
-
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
         app.listen(PORT, console.log("Server started on port: ", PORT));
